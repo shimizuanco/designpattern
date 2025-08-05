2c1ec83d922e884cf4630bd2977ae0eeefe84f20
shimizuanco <shimizu.an.co@gmail.com>
2025-08-05 13:47:20 +0900

    Mainをpatternからコピー

--- designpattern/src/com/hakotrain/designpattern/iterator/Main.java
+++ designpattern/src/com/hakotrain/designpattern/iterator/Main.java
@@ -1,5 +1,17 @@
 package com.hakotrain.designpattern.iterator;
 
 public class Main {
-
+	public static void main(String[] args) {
+		BookShelf bookShelf = new BookShelf(5);
+		
+		bookShelf.appendBook(new Book("吾輩は猫である"));
+		bookShelf.appendBook(new Book("吾輩も猫である"));
+		bookShelf.appendBook(new Book("吾輩は犬である"));
+		bookShelf.appendBook(new Book("吾輩も犬である"));
+		
+		for(Book book : bookShelf) {
+			String name = book.name();
+			System.out.println("Book title : " + name);
+		}
+	}
 }
