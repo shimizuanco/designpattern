d17a058f3bbdb910f3f4a8166d8f442692d1211b
shimizuanco <shimizu.an.co@gmail.com>
2025-08-16 13:08:57 +0900

    BookShelf : 例外を追加

--- designpattern/src/com/hakotrain/designpattern/iterator/BookShelf.java
+++ designpattern/src/com/hakotrain/designpattern/iterator/BookShelf.java
@@ -1,36 +1,53 @@
 package com.hakotrain.designpattern.iterator;
 
-import java.util.Iterator;
-
 public class BookShelf implements Iterable<Book> {
 	
 	private Book[] books;
+	private int maxSize;
 	private int last;
 	
 	public BookShelf(int maxSize) {
+		// BookShelfで管理できる本の数は、1以上であること
 		if(maxSize < 1) {
-			throw new IllegalArgu
+			throw new IllegalArgumentException("BookShelfの最大サイズは、1以上を指定してください");
 		}
+		
 		books = new Book[maxSize];
+		this.maxSize = maxSize;
 		last = 0;
 	}
 	
 	@Override
-	public Iterator<Book> iterator() {
+	public BookShelfIterator iterator() {
 		return new BookShelfIterator(this);
 	}
 	
 	public Book getBookAt(int index) {
+		// 現在管理されている本のindex値は、0以上last以下であること
 		if(index < 0 || index >= last) {
-			throw new IllegalArgumentException("indexの値は、0以上" + last + "以下を指定してください。" );
+			throw new IllegalArgumentException("取り出す本を識別するindex値は、0以上" + last + "以下を指定してください" );
 		}
 		
 		return books[index];
 	}
 	
 	public void appendBook(Book book) {
+		// 追加するBookオブジェクトは、nullでないこと
+		if(book == null) {
+			throw new IllegalArgumentException("Bookオブジェクトは、null以外を指定してください" );
+		}
+		
+		// 管理する本の数が配列のサイズを超えないこと
+		if(last >= books.length) {
+			throw new IllegalArgumentException("本の数が上限を超えました" );
+		}
+
 		books[last] = book;
 		last++;
+	}
+	
+	public int getMaxSize() {
+		return maxSize;
 	}
 	
 	public int getLast() {
